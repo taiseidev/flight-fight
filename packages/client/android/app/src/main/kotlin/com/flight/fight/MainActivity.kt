de282e06072238f3518f39979a4f619cbd3dac53
onishi taisei <taisei.developer@gmail.com>
2023-12-25 23:17:35 +0900

    🚀 feat: Add client project

---
+++ packages/client/android/app/src/main/kotlin/com/flight/fight/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flight.fight.client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ packages/client/lib/game/game.dart
@@ -0,0 +1,5 @@
+export 'client.dart';
+export 'components/components.dart';
+export 'cubit/cubit.dart';
+export 'entities/entities.dart';
+export 'view/view.dart';
